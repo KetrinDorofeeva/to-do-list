05aa08f26832e184b81e1a03924a670928a4ff01
Екатерина Дорофеева <multiveb@mail.ru>
2023-05-04 16:37:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //publicPath: '/to-do-list/',
+  base: '/to-do-list/',
   plugins: [
     vue(),
     VitePWA({
