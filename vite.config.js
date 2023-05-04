6f6f92eb2cfba3144a8fcf150c0ace04bc59473e
Екатерина Дорофеева <multiveb@mail.ru>
2023-05-04 16:03:36 +0300

    Комментирование в vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import {VitePWA} from "vite-plugin-pwa";
+//import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/to-do-list/',
   plugins: [
     vue(),
-    VitePWA({
-      injectRegister: 'auto'
-    })
+    // VitePWA({
+    //   injectRegister: 'auto'
+    // })
   ],
 })
