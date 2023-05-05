ead31ae4cd35208850b7d7704c2dc881873d2d0c
Екатерина Дорофеева <multiveb@mail.ru>
2023-05-05 09:42:15 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -18,7 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHashHistory(process.env.NODE_ENV === 'production' ? '/to-do-list/' : '/'),
     //history: createWebHistory(process.env.NODE_ENV === 'production' ? '/to-do-list/' : '/'),
     routes
 })
