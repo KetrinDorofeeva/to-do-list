b1517254e38d1a285784229d4b49844f5581eff9
Екатерина Дорофеева <multiveb@mail.ru>
2023-05-05 09:00:08 +0300

    base в vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import {VitePWA} from "vite-plugin-pwa";
+//import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL,
   plugins: [
     vue(),
-    VitePWA({
-      injectRegister: 'auto'
-    })
+    // VitePWA({
+    //   injectRegister: 'auto'
+    // })
   ],
+  base:
+      process.env.NODE_ENV === "production"
+          ? "/to-do-list/"
+          : "/",
 })
